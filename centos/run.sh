74b7165afd47b28bb9811ddb9d221aa794d42970
wzh <wangzheng422@gmail.com>
2018-07-11 15:44:36 +0800

    centos

--- centos/run.sh
+++ centos/run.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-docker run --network=elastic --rm -it --name=centos -v /root/work/centos:/mnt centos:wzh bash -c "yum -y update; cd /mnt; reposync -d ./"
+docker run --rm -it --name=centos -v /root/work/centos:/mnt centos:wzh bash -c "yum -y update; cd /mnt; reposync -d ./"
