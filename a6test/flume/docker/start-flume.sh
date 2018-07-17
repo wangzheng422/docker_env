705aa07a0eb0e8b66b184d520b5ff1fa5a7a4cac
wzh <wangzheng422@gmail.com>
2018-07-17 22:31:01 +0800

    flume

--- a6test/flume/docker/start-flume.sh
+++ a6test/flume/docker/start-flume.sh
@@ -11,5 +11,5 @@ flume-ng agent \
   -c ${FLUME_CONF_DIR} \
   -f ${FLUME_CONF_DIR}/flume.conf \
   -n ${FLUME_AGENT_NAME} \
-  -Dflume.root.logger=INFO,console \
+  -Dflume.root.logger=DEBUG,console \
   $*
