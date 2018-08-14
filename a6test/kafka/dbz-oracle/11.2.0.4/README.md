52bd059c01883a10ad2923de3ff32b8e8c09c740
wzh <wangzheng422@gmail.com>
2018-08-14 14:40:27 +0800

    oracle dbz

---
+++ a6test/kafka/dbz-oracle/11.2.0.4/README.md
@@ -0,0 +1,39 @@
+# dbz for oracle demo steps
+
+## install
+
+follow <https://github.com/debezium/docker-images/tree/master/connect/0.9>
+
+you need to download the driver to ./tmp
+
+then, run deploy.sh
+
+## initialization
+
+follow <https://github.com/debezium/oracle-vagrant-box/blob/master/setup.sh>
+
+run this setup.sh in oracledb
+
+## create kafka connector
+
+```bash
+docker-compose exec dbz-connect curl -X POST -H "Content-Type: application/json" \
+    --data '{ "name": "inventory-connector", "config": { "connector.class": "io.debezium.connector.oracle.OracleConnector", "tasks.max": "1", "database.server.name": "oracledb", "database.hostname": "oracledb", "database.port": "1521", "database.user": "c##xstrm", "database.password": "xs", "database.dbname": "orcl", "database.out.server.name": "dbzxout", "database.history.kafka.bootstrap.servers": "kafka1:9092", "database.history.kafka.topic": "schema-changes.inventory" } }' \
+    http://dbz-connect:8083/connectors
+```
+
+## feed some data
+
+follow <https://github.com/debezium/debezium-examples/blob/master/tutorial/debezium-with-oracle-jdbc/init/inventory.sql>
+
+feed this data into database, remember use below sql to connect db and run the sql
+
+```sql
+sqlplus debezium/dbz@//localhost:1521/orcl
+```
+
+## result
+
+now you can see the result in kafka console
+
+![alt text](https://github.com/wangzheng422/docker_env/raw/master/a6test/docs/oracle-dbz.png)
--- a6test/kafka/dbz-oracle/11.2.0.4/setup.sh
+++ a6test/kafka/dbz-oracle/11.2.0.4/setup.sh
@@ -1,9 +1,11 @@
 
-
+# as root
+su -
 mkdir -p /opt/oracle/oradata/recovery_area
 mkdir -p /opt/oracle/oradata/ORCLCDB
-chown oracle:dba /opt/oracle
+chown -R oracle:dba /opt/oracle
 
+su - oracle
 
 sqlplus /nolog <<- EOF
 	CONNECT sys/oracle AS SYSDBA
@@ -16,13 +18,16 @@ sqlplus /nolog <<- EOF
 	alter database open;
         -- Should show "Database log mode: Archive Mode"
 	archive log list
+	exit;
+EOF
 
+sqlplus sys/oracle@//localhost:1521/orcl as sysdba <<- EOF
     CREATE TABLESPACE xstream_adm_tbs DATAFILE '/opt/oracle/oradata/ORCLCDB/xstream_adm_tbs.dbf'
 	  SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
+	exit;
+EOF
 
-    CREATE TABLESPACE xstream_adm_tbs DATAFILE '/opt/oracle/oradata/ORCLCDB/ORCLPDB1/xstream_adm_tbs.dbf'
-	  SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
-
+sqlplus sys/oracle@//localhost:1521/orcl as sysdba <<- EOF
     CREATE USER c##xstrmadmin IDENTIFIED BY xsa
 	  DEFAULT TABLESPACE xstream_adm_tbs
 	  QUOTA UNLIMITED ON xstream_adm_tbs;
@@ -38,30 +43,39 @@ sqlplus /nolog <<- EOF
 	END;
 	/
 
+	exit;
+EOF
 
+sqlplus sys/oracle@//localhost:1521/orcl as sysdba <<- EOF
     CREATE USER debezium IDENTIFIED BY dbz;
 	GRANT CONNECT TO debezium;
 	GRANT CREATE SESSION TO debezium;
 	GRANT CREATE TABLE TO debezium;
 	GRANT CREATE SEQUENCE TO debezium;
 	ALTER USER debezium QUOTA 100M ON users;
+	exit;
+EOF
 
+sqlplus sys/oracle@//localhost:1521/orcl as sysdba <<- EOF
     CREATE TABLESPACE xstream_tbs DATAFILE '/opt/oracle/oradata/ORCLCDB/xstream_tbs.dbf'
 	  SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
+	exit;
+EOF
 
+sqlplus sys/oracle@//localhost:1521/orcl as sysdba <<- EOF
     CREATE USER c##xstrm IDENTIFIED BY xs
 	  DEFAULT TABLESPACE xstream_tbs
 	  QUOTA UNLIMITED ON xstream_tbs;
-
 
     GRANT CREATE SESSION TO c##xstrm;
     GRANT SELECT ON V_$DATABASE to c##xstrm ;
     GRANT FLASHBACK ANY TABLE TO c##xstrm;
 
+	exit;
+EOF
 
 
-    CONNECT c##xstrmadmin/xsa 
-
+sqlplus c##xstrmadmin/xsa@//localhost:1521/orcl <<- EOF
     DECLARE
 	  tables  DBMS_UTILITY.UNCL_ARRAY;
 	  schemas DBMS_UTILITY.UNCL_ARRAY;
@@ -74,111 +88,15 @@ sqlplus /nolog <<- EOF
 	    schema_names    =>  schemas);
 	END;
 	/
+	exit;
+EOF
 
-    CONNECT sys/oracle AS SYSDBA
+sqlplus sys/oracle@//localhost:1521/orcl as sysdba <<- EOF
     BEGIN
         DBMS_XSTREAM_ADM.ALTER_OUTBOUND(
         server_name  => 'dbzxout',
         connect_user => 'c##xstrm');
     END;
     /
-
-
-
-EOF
-
-# Create XStream admin user
-sqlplus sys/oracle@//localhost:1521/orcl as sysdba <<- EOF
-	CREATE TABLESPACE xstream_adm_tbs DATAFILE '/opt/oracle/oradata/ORCLCDB/xstream_adm_tbs.dbf'
-	  SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
 	exit;
-EOF
-sqlplus sys/oracle@//localhost:1521/ORCLPDB1 as sysdba <<- EOF
-	CREATE TABLESPACE xstream_adm_tbs DATAFILE '/opt/oracle/oradata/ORCLCDB/ORCLPDB1/xstream_adm_tbs.dbf'
-	  SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
-	exit;
-EOF
-sqlplus sys/oracle@//localhost:1521/ORCLCDB as sysdba <<- EOF
-	CREATE USER c##xstrmadmin IDENTIFIED BY xsa
-	  DEFAULT TABLESPACE xstream_adm_tbs
-	  QUOTA UNLIMITED ON xstream_adm_tbs
-	  CONTAINER=ALL;
-
-	GRANT CREATE SESSION, SET CONTAINER TO c##xstrmadmin CONTAINER=ALL;
-
-	BEGIN
-	   DBMS_XSTREAM_AUTH.GRANT_ADMIN_PRIVILEGE(
-	      grantee                 => 'c##xstrmadmin',
-	      privilege_type          => 'CAPTURE',
-	      grant_select_privileges => TRUE
-	   );
-	END;
-	/
-
-	exit;
-EOF
-
-# Create test user
-sqlplus sys/oracle@//localhost:1521/ORCLPDB1 as sysdba <<- EOF
-	CREATE USER debezium IDENTIFIED BY dbz;
-	GRANT CONNECT TO debezium;
-	GRANT CREATE SESSION TO debezium;
-	GRANT CREATE TABLE TO debezium;
-	GRANT CREATE SEQUENCE TO debezium;
-	ALTER USER debezium QUOTA 100M ON users;
-
-	exit;
-EOF
-
-# Create XStream user
-sqlplus sys/oracle@//localhost:1521/ORCLCDB as sysdba <<- EOF
-  CREATE TABLESPACE xstream_tbs DATAFILE '/opt/oracle/oradata/ORCLCDB/xstream_tbs.dbf'
-	  SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
-  exit;
-EOF
-sqlplus sys/oracle@//localhost:1521/ORCLPDB1 as sysdba <<- EOF
-  CREATE TABLESPACE xstream_tbs DATAFILE '/opt/oracle/oradata/ORCLCDB/ORCLPDB1/xstream_tbs.dbf'
-	  SIZE 25M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;
-  exit;
-EOF
-sqlplus sys/oracle@//localhost:1521/ORCLCDB as sysdba <<- EOF
-  CREATE USER c##xstrm IDENTIFIED BY xs
-	  DEFAULT TABLESPACE xstream_tbs
-	  QUOTA UNLIMITED ON xstream_tbs
-	  CONTAINER=ALL;
-
-  GRANT CREATE SESSION TO c##xstrm CONTAINER=ALL;
-  GRANT SET CONTAINER TO c##xstrm CONTAINER=ALL;
-  GRANT SELECT ON V_\$DATABASE to c##xstrm CONTAINER=ALL;
-  GRANT FLASHBACK ANY TABLE TO c##xstrm CONTAINER=ALL;
-  exit;
-EOF
-
-# Create XStream Outbound server
-sqlplus c##xstrmadmin/xsa@//localhost:1521/ORCLCDB <<- EOF
-	DECLARE
-	  tables  DBMS_UTILITY.UNCL_ARRAY;
-	  schemas DBMS_UTILITY.UNCL_ARRAY;
-	BEGIN
-	    tables(1)  := NULL;
-	    schemas(1) := 'debezium';
-	  DBMS_XSTREAM_ADM.CREATE_OUTBOUND(
-	    server_name     =>  'dbzxout',
-	    table_names     =>  tables,
-	    schema_names    =>  schemas);
-	END;
-	/
-
-	exit;
-EOF
-
-sqlplus sys/oracle@//localhost:1521/ORCLCDB as sysdba <<- EOF
-  BEGIN
-    DBMS_XSTREAM_ADM.ALTER_OUTBOUND(
-      server_name  => 'dbzxout',
-      connect_user => 'c##xstrm');
-  END;
-  /
-
-  exit;
 EOF
