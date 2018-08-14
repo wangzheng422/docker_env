921fc20637b76e9f2f87ab708333b4f115faad59
wzh <wangzheng422@gmail.com>
2018-08-14 18:42:14 +0800

    oracle dbz

--- a6test/kafka/dbz-oracle/deploy.sh
+++ a6test/kafka/dbz-oracle/deploy.sh
@@ -10,3 +10,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -f 12.2.Dockerfile -t dbz-oracle-12-2:wzh ${DIR}/
 docker build -f 11.2.Dockerfile -t dbz-oracle-11-2:wzh ${DIR}/
+docker build -t dbz-oracle:wzh ${DIR}/
