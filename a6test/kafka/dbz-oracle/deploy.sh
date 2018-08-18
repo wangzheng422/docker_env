931d7f6e44a7593783e7d885baf5d6ee1d9da62e
wzh <wangzheng422@gmail.com>
2018-08-18 15:27:43 +0800

    oracle dbz

--- a6test/kafka/dbz-oracle/deploy.sh
+++ a6test/kafka/dbz-oracle/deploy.sh
@@ -6,6 +6,14 @@ set -x
 # SCRIPT=$(readlink -f "$0")
 # SCRIPTPATH=$(dirname "$SCRIPT")
 
+# mvn clean install -DskipITs
+# mvn clean install -pl debezium-connector-oracle -am -Poracle -DskipITs  -Dinstantclient.dir=/root/work/docker_env/a6test/kafka/dbz-oracle/tmp/instantclient_12_2
+
+cp /root/work/debezium/debezium-core/target/debezium-core-0.9.0-SNAPSHOT.jar .tmp/
+cp /root/work/debezium-incubator/debezium-connector-oracle/target/debezium-connector-oracle-0.9.0-SNAPSHOT.jar .tmp/
+cp /root/work/debezium/debezium-ddl-parser/target/debezium-ddl-parser-0.9.0-SNAPSHOT.jar .tmp/
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # docker build -f 12.2.Dockerfile -t dbz-oracle-12-2:wzh ${DIR}/
