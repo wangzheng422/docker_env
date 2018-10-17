3198be1933574348a8d3db495cd5bb3abb8c3d3b
wzh <wangzheng422@gmail.com>
2018-10-17 10:46:01 +0800

    teiid

--- a6test/teiid/entrypoint.sh
+++ a6test/teiid/entrypoint.sh
@@ -9,10 +9,14 @@ echo $PREFIX"Bootstraping teiid..."
 if [ "$SERVER_ROLE" = "master" ]; then
     echo $PREFIX"Will start as master"
 
+    cp -f /opt/jboss/wildfly/domain/configuration/host-master.xml /opt/jboss/wildfly/domain/configuration/host.xml
+
     /opt/jboss/wildfly/bin/domain.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 
 else
   echo $PREFIX"Will start as slave"
+
+  cp -f /opt/jboss/wildfly/domain/configuration/host-slave.xml /opt/jboss/wildfly/domain/configuration/host.xml
 
   /opt/jboss/wildfly/bin/domain.sh --master-address=${MASTER_ADDR} -b 0.0.0.0 -bmanagement 0.0.0.0
 
