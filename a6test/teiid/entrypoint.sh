0ef6abdbf74ee9ac61b26a035c3030669c2d2dc0
wzh <wangzheng422@gmail.com>
2018-10-17 15:43:58 +0800

    teiid

--- a6test/teiid/entrypoint.sh
+++ a6test/teiid/entrypoint.sh
@@ -18,6 +18,8 @@ else
 
   cp -f /opt/jboss/wildfly/domain/configuration/host-slave.1.xml /opt/jboss/wildfly/domain/configuration/host.xml
 
+  sed -i "s|{{slave_name}}|${SLAVE_NAME}|g" /opt/jboss/wildfly/domain/configuration/host.xml
+
   sleep 60
 
   /opt/jboss/wildfly/bin/domain.sh --master-address=${MASTER_ADDR} -b 0.0.0.0 -bmanagement 0.0.0.0
