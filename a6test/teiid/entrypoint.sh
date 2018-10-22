3b30fb47e496ca85297416647911090ed38d1d98
wzh <wangzheng422@gmail.com>
2018-10-22 21:54:20 +0800

    teiid

--- a6test/teiid/entrypoint.sh
+++ a6test/teiid/entrypoint.sh
@@ -24,6 +24,9 @@ else
 
   /opt/jboss/wildfly/bin/domain.sh --master-address=${MASTER_ADDR} -b 0.0.0.0 -bmanagement 0.0.0.0
 
+  sleep 60
+  /opt/jboss/wildfly/bin/jboss-cli.sh --file=/opt/jboss/wildfly/bin/scripts/teiid-domain-mode-install.cli
+
 fi
 
 wait || :
