bb2e78e64b5d99cfb8d38ec5c9bc14a19f64cfe2
wzh <wangzheng422@gmail.com>
2018-10-31 16:18:38 +0800

    teiid

---
+++ a6test/teiid/deploy.0.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# source config.sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+git pull
+
+docker build -f 9.0.Dockerfile -t teiid:wzh ${DIR}/
+
+docker image prune -f
