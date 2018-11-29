44da696ce64526ebc8a16933d0b5315fca8425d3
wzh <wangzheng422@gmail.com>
2018-11-29 14:47:01 +0800

    teiid

--- a6test/teiid/env.oracle/run.sh
+++ a6test/teiid/env.oracle/run.sh
@@ -5,11 +5,11 @@ set -x
 
 docker-compose up -d -V
 
-sleep 10
+# sleep 10
 
-docker-compose exec teiid-master /opt/jboss/wildfly/bin/add-user.sh -u root -p root -e
+# docker-compose exec teiid-master /opt/jboss/wildfly/bin/add-user.sh -u root -p root -e
 
-docker-compose exec teiid-master /opt/jboss/wildfly/bin/add-user.sh -a -u app -p app -e
+# docker-compose exec teiid-master /opt/jboss/wildfly/bin/add-user.sh -a -u app -p app -e
 
 # docker exec -it filebeat
 # docker-compose exec filebeat bash
