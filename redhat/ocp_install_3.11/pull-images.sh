6aeb04afba2c2490011b3b7bc0a376cf942dca62
wzh <wangzheng422@gmail.com>
2019-02-14 18:07:20 +0800

    rh

---
+++ redhat/ocp_install_3.11/pull-images.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+dummy=$1
+
+# mkdir -p tmp
+
+source config.sh
+
+while read -r line; do
+    if [[ "$line" =~ [^[:space:]] ]]; then
+        # read -ra images <<<"$line"
+        # echo "docker save ${images[0]} | gzip -c > tmp/${images[1]}"
+        # if [ "$dummy" != "dummy" ]; then
+        #     docker save ${images[0]} | gzip -c > tmp/${images[1]}
+        # fi
+        echo $line;
+    fi
+done <<< "$ose3-images"
+
+docker image prune -f
+
+
+
