fa6085203f051cc018eb5e10c2d6a26da9b15a8c
wzh <wangzheng422@gmail.com>
2019-10-18 11:03:21 +0800

    rh

---
+++ redhat/ocp4/files/4.2/config.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export tag="4.2"
+export private_repo="vm.redhat.ren"
+export major_tag="4.2"
+# https://access.redhat.com/articles/2834301
+# docker search rhgs3 | grep redhat.io | awk '{print $2}'
+# search for openshift, openshift3, fuse7, dotnet, jboss
+
+## 后续导入的时候，要用2个版本号
+ose4_images=$(cat << EOF
+quay.io/openshift/origin-cli:$tag
+quay.io/openshift/origin-cli-artifacts:$tag
+quay.io/openshift/origin-cluster-samples-operator:$tag
+quay.io/openshift/origin-baremetal-installer:$tag
+quay.io/openshift/origin-installer:$tag
+quay.io/openshift/origin-installer-artifacts:$tag
+quay.io/openshift/origin-jenkins:$tag
+quay.io/openshift/origin-jenkins-agent-nodejs:$tag
+quay.io/openshift/origin-jenkins-agent-maven:$tag
+quay.io/openshift/origin-tests:$tag
+quay.io/openshift/origin-must-gather:$tag
+
+
+EOF
+)
+
+
