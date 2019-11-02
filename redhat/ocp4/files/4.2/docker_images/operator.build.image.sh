03de978b36a7cb568aef836036c9d06d2262ad7a
wzh <wangzheng422@gmail.com>
2019-11-02 12:53:44 +0800

    rh

--- redhat/ocp4/files/4.2/docker_images/operator.build.image.sh
+++ redhat/ocp4/files/4.2/docker_images/operator.build.image.sh
@@ -5,21 +5,31 @@ set -x
 
 cd /data/ocp4/operator/
 
-podman build -f /data/ocp4/custom-registry.Dockerfile -t registry.redhat.ren/ocp-operator/custom-registry:all ./
+tar zxf manifests.tgz
+
+podman build --no-cache -f /data/ocp4/custom-registry.Dockerfile -t registry.redhat.ren/ocp-operator/custom-registry:all ./
+
+podman push registry.redhat.ren/ocp-operator/custom-registry:all 
 
 podman image save registry.redhat.ren/ocp-operator/custom-registry:all | pigz -c > custom-registry.all.tgz
 
 /bin/rm -rf ./manifests/community-operators.*
 
-podman build -f /data/ocp4/custom-registry.Dockerfile -t registry.redhat.ren/ocp-operator/custom-registry:certified ./
+podman build --no-cache -f /data/ocp4/custom-registry.Dockerfile -t registry.redhat.ren/ocp-operator/custom-registry:certified ./
 
-podman image save registry.redhat.ren/ocp-operator/custom-registry:community | pigz -c > custom-registry.certified.tgz
+podman push registry.redhat.ren/ocp-operator/custom-registry:certified
+
+podman image save registry.redhat.ren/ocp-operator/custom-registry:certified | pigz -c > custom-registry.certified.tgz
 
 /bin/rm -rf ./manifests/certified-operators.*
 
-podman build -f /data/ocp4/custom-registry.Dockerfile -t registry.redhat.ren/ocp-operator/custom-registry:redhat ./
+podman build --no-cache -f /data/ocp4/custom-registry.Dockerfile -t registry.redhat.ren/ocp-operator/custom-registry:redhat ./
 
-podman image save registry.redhat.ren/ocp-operator/custom-registry:community | pigz -c > custom-registry.redhat.tgz
+podman push registry.redhat.ren/ocp-operator/custom-registry:redhat
+
+podman image save registry.redhat.ren/ocp-operator/custom-registry:redhat | pigz -c > custom-registry.redhat.tgz
+
+cd /data/ocp4
 
 # find ./operator -type f | xargs grep "image: " | sed 's/^.*image: //' | sort | uniq | grep -e '^.*\/.*:.*' | grep -v '\\n' | sed s/"'"//g | sed 's/\"//g' | sort | uniq >  /data/ocp4/operator.image.list
 
