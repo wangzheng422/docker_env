e80c6b47d0488cb1105ec44f9df1535710ed3c6a
wzh <wangzheng422@gmail.com>
2020-06-08 15:30:04 +0800

    ocp 4.4

--- redhat/ocp4/4.4/scripts/build.dist.sh
+++ redhat/ocp4/4.4/scripts/build.dist.sh
@@ -6,11 +6,11 @@ set -x
 # export BUILDNUMBER="4.2.13"
 # stable 4.3.5
 build_number_list=$(cat << EOF
-4.4.4
+4.4.7
 EOF
 )
 
-export var_date='2020-05-16'
+export var_date='2020-06-08'
 echo $var_date
 export var_major_version='4.4'
 echo ${var_major_version}
