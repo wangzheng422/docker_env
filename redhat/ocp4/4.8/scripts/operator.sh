bde74674cbe60106bbb4cd447966c937a13a32c6
wzh <wangzheng422@gmail.com>
2021-08-18 22:42:56 +0800

    ocp 4.8

--- redhat/ocp4/4.8/scripts/operator.sh
+++ redhat/ocp4/4.8/scripts/operator.sh
@@ -3,9 +3,38 @@
 set -e
 set -x
 
+usage() { 
+  echo "
+Usage: $0  [-m <ocp major version for operator hub, like '4.6'>]  
+Example: $0 -m 4.6   
+  " 1>&2
+  exit 1 
+}
+
+while getopts ":m:" o; do
+    case "${o}" in
+
+        m)
+            var_major_version=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+if [  [ -z "${var_major_version}" ] ; then
+    usage
+fi
+
+echo "var_major_version = ${var_major_version}"
+
+
+
 var_date=$(date '+%Y.%m.%d.%H%M')
 echo $var_date
-export var_major_version='4.7'
+# export var_major_version='4.8'
 echo ${var_major_version}
 
 # https://docs.openshift.com/container-platform/4.6/operators/admin/olm-restricted-networks.html
@@ -31,7 +60,7 @@ cd /data/ocp4
 #     --manifests-only
 
 skopeo copy \
-    docker://registry.redhat.io/redhat/redhat-operator-index:v4.7 \
+    docker://registry.redhat.io/redhat/redhat-operator-index:v${var_major_version} \
     docker://quay.io/wangzheng422/operator-catalog:redhat-${var_major_version}-$var_date
 
 # VAR_DIR=`find /tmp -type d -regex '^/tmp/[0-9]+$' `
@@ -47,7 +76,7 @@ skopeo copy \
 #     --to=docker.io/wangzheng422/operator-catalog:certified-${var_major_version}-$var_date  
 
 skopeo copy \
-    docker://registry.redhat.io/redhat/certified-operator-index:v4.7 \
+    docker://registry.redhat.io/redhat/certified-operator-index:v${var_major_version} \
     docker://quay.io/wangzheng422/operator-catalog:certified-${var_major_version}-$var_date
 
 # oc adm catalog build --filter-by-os='linux/amd64' \
@@ -65,7 +94,7 @@ skopeo copy \
 #     --to=docker.io/wangzheng422/operator-catalog:redhat-marketplace-${var_major_version}-$var_date  
 
 skopeo copy \
-    docker://registry.redhat.io/redhat/redhat-marketplace-index:v4.7 \
+    docker://registry.redhat.io/redhat/redhat-marketplace-index:v${var_major_version} \
     docker://quay.io/wangzheng422/operator-catalog:redhat-marketplace-${var_major_version}-$var_date
 
 echo "quay.io/wangzheng422/operator-catalog:redhat-${var_major_version}-$var_date "
