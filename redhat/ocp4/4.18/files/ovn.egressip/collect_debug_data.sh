b12325cee471627186b390d95513b59c51d60a80
wangzheng422 <wangzheng422@gmail.com>
2026-01-24 10:39:07 +0800

    debug

--- redhat/ocp4/4.18/files/ovn.egressip/collect_debug_data.sh
+++ redhat/ocp4/4.18/files/ovn.egressip/collect_debug_data.sh
@@ -59,24 +59,24 @@ for NODE_KEY in "${NODES[@]}"; do
     
     # Dump OVS Bridge/Port Config
     echo "  - Collecting ovs-vsctl show..."
-    oc exec -n "$NAMESPACE" "$NODE_POD" -c ovnkube-node -- ovs-vsctl show > "$NODE_DIR/ovs_vsctl_show.txt"
+    oc exec -n "$NAMESPACE" "$NODE_POD" -c ovn-controller -- ovs-vsctl show > "$NODE_DIR/ovs_vsctl_show.txt"
     
     # Dump OpenFlows (huge output usually) - SKIPPED as per request
     # echo "  - Collecting ovs-ofctl dump-flows..."
-    # oc exec -n "$NAMESPACE" "$NODE_POD" -c ovnkube-node -- ovs-ofctl -O OpenFlow13 dump-flows br-int > "$NODE_DIR/ovs_flows.txt"
+    # oc exec -n "$NAMESPACE" "$NODE_POD" -c ovn-controller -- ovs-ofctl -O OpenFlow13 dump-flows br-int > "$NODE_DIR/ovs_flows.txt"
     
     # Dump Conntrack (can be very large, filtering for the relevant IPs if known would be better, but dumping all for analysis)
     # Warning: This might be truncated if too large
     # echo "  - Collecting conntrack entries..."
-    # oc exec -n "$NAMESPACE" "$NODE_POD" -c ovnkube-node -- ovs-appctl dpctl/dump-conntrack > "$NODE_DIR/conntrack.txt"
+    # oc exec -n "$NAMESPACE" "$NODE_POD" -c ovn-controller -- ovs-appctl dpctl/dump-conntrack > "$NODE_DIR/conntrack.txt"
     
     # Dump Interface list from OS (using chroot /host usually required, but let's try via container if mapped, 
     # usually ovnkube-node is privileged but might not have host network namespace tools directly in path.
     # A safer bet for IP/Route is 'oc debug node' but that requires interactive or carefully scripted input.
     # We will try basic ip addr inside the container - often it shares host net ns)
     echo "  - Collecting IP addresses and Routes..."
-    oc exec -n "$NAMESPACE" "$NODE_POD" -c ovnkube-node -- ip a > "$NODE_DIR/ip_a.txt"
-    oc exec -n "$NAMESPACE" "$NODE_POD" -c ovnkube-node -- ip route > "$NODE_DIR/ip_route.txt"
+    oc exec -n "$NAMESPACE" "$NODE_POD" -c ovn-controller -- ip a > "$NODE_DIR/ip_a.txt"
+    oc exec -n "$NAMESPACE" "$NODE_POD" -c ovn-controller -- ip route > "$NODE_DIR/ip_route.txt"
     
 done
 
